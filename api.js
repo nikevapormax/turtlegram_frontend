12838c03956322a48940e998318dd44cfa3fcc0f
Daegeun Jeong <wjdeorms27@naver.com>
2022-05-17 19:19:15 +0900

    Feat: signin - just fetch

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-async function handleSignin() { // async를 붙여주어 '비동기함수'임을 알려줌
+async function handleSignup() { // async를 붙여주어 '비동기함수'임을 알려줌
 
     // email과 password의 값을 가져옴
     const signupData = {
@@ -21,4 +21,20 @@ async function handleSignin() { // async를 붙여주어 '비동기함수'임을
         alert(response.status);
         window.location.reload();
     }
+}
+
+async function handleSignin() { // async를 붙여주어 '비동기함수'임을 알려줌
+
+    // email과 password의 값을 가져옴
+    const signinData = {
+        email: document.getElementById('floatingInput').value,
+        password: document.getElementById('floatingPassword').value
+    }
+    // await을 붙여주어 해당 부분이 '비동기' 부분임을 알려줌
+    const response = await fetch('http://127.0.0.1:5000/signin', {
+        method: "POST",
+        body: JSON.stringify(signinData)
+    })
+
+    console.log(response)
 }
