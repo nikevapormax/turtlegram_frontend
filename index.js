a89e6f898b08687fc0e26ad2d7a5b47d093b9b44
Daegeun Jeong <wjdeorms27@naver.com>
2022-05-19 00:48:28 +0900

    Feat: get article's info & append it to ul

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log('index.js')
+async function loadArticles() {
+    articles = await getArticles();
 
+    const article_list = document.getElementById('articles')
+    articles.forEach(article => {
+        console.log(article)
+        const newArticle = document.createElement('li');
+        newArticle.setAttribute('id', article._id)
+        newArticle.innerText = article.title;
+        article_list.appendChild(newArticle)
+
+    });
+}
+
+
+loadArticles();
 getName();
